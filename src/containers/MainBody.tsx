9207d5d90b0a192e2cc081233d10383efc96ebd2
Aditya <officialdesaiaditya@gmail.com>
2023-11-11 17:06:11 +0530

    Add qrcode

--- src/containers/MainBody.tsx
+++ src/containers/MainBody.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import InputContainer from "./InputContainer";
+import { QRCodeSVG } from "qrcode.react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { toPng } from "react-svg-to-image";
 
 const MainBody = () => {
+	const [qr, setQr] = useState<boolean>(false);
 	const [inputValues, setInputValues] = useState<any>({});
 
 	const handleChange = (e: any) => {
@@ -12,11 +17,27 @@ const MainBody = () => {
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
+		setQr(true);
 		console.log({ inputValues });
 	};
 
+	const handleDownload = () => {
+		const canvas = document.getElementById("qrcode-gen");
+		const pngUrl = canvas
+			.toDataURL("image/png")
+			.replace("image/png", "image/octet-stream");
+		let downloadLink = document.createElement("a");
+		downloadLink.href = pngUrl;
+		downloadLink.download = `qrcode.png`;
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+		document.body.removeChild(downloadLink);
+
+		// console.log("download");---=
+	};
+
 	return (
-		<div className="h-[20rem] w-2/3 border-2 border-gray-300 mx-auto my-2 text-center">
+		<div className="w-1/3 border-2 border-gray-300 mx-auto my-2 text-center shadow-lg">
 			<form
 				className="flex flex-col items-center my-2"
 				onSubmit={handleSubmit}
@@ -60,6 +81,24 @@ const MainBody = () => {
 					Get QR
 				</button>
 			</form>
+			{qr && (
+				<div className="flex flex-col items-center ease-in-out">
+					<QRCodeSVG
+						id="qrcode-gen"
+						value={inputValues.url}
+						size={inputValues.size}
+					/>
+					<button
+						className="my-4 p-2 bg-blue-600 rounded-lg text-white hover:bg-blue-700 w-36"
+						onClick={handleDownload}
+					>
+						<span className="mr-2">
+							<FontAwesomeIcon icon={faDownload} />
+						</span>
+						Download
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
