92521d2a766c404c1d7b011d45c38356f253350d
Aditya <officialdesaiaditya@gmail.com>
2023-11-19 14:03:11 +0530

    ﻿Final commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import Routes from "./routes/Route";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+const App = lazy(() => import("./App"));
+const Home = lazy(() => import("./containers/Home"));
+const MainBody = lazy(() => import("./containers/MainBody"));
 
 const root = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(root).render(
 	<Suspense fallback={<span>Loading...</span>}>
-		<RouterProvider router={Routes} />
+		<HashRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="/" element={<Home />} />
+					<Route path="generate-qr" element={<MainBody />} />
+					<Route
+						path="*"
+						element={<span>Some error occured!! Check url</span>}
+					/>
+				</Route>
+			</Routes>
+		</HashRouter>
 	</Suspense>
 );
