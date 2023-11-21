aa7b3bd06777f4c4da5fe2d010cd3b815f930053
Aditya <officialdesaiaditya@gmail.com>
2023-11-21 10:27:44 +0530

    Updates

---
+++ TypeWriter.b6588a36.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire00c8;(0,e.register)("4dPpb",function(t,r){Object.defineProperty(t.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(t.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=e("acw62"),i=({text:e,delay:t,className:r,infinite:i})=>{let[s,u]=(0,l.useState)(""),[n,c]=(0,l.useState)(0);return(0,l.useEffect)(()=>{if(n<e.length){let r=setTimeout(()=>{u(t=>t+e[n]),c(e=>e+1)},t);return()=>clearTimeout(r)}i&&(c(0),u(""))},[n,t,e]),/*#__PURE__*/(0,a.jsx)("span",{className:r,children:s})}});//# sourceMappingURL=TypeWriter.b6588a36.js.map
+
+//# sourceMappingURL=TypeWriter.b6588a36.js.map
